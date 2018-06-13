768b6654cacbb44ced59bdf838634e76ff8a9a97
Pedro Melo <phenriqmelo99@gmail.com>
2018-06-13 13:58:55 +0000

    CSS botão/input

--- src/App.vue
+++ src/App.vue
@@ -30,5 +30,30 @@ background-size: 100%;
 background-repeat: no-repeat;
 }
 
+#mid{
+    text-align: center;
+}
 
+button{
+    width: 100px;
+    height: 55px;
+    background-color: #80d4ff;
+    border-style: none;
+    font-size: 16px;
+    font-weight: 600;
+}
+
+button:hover{
+    transition-duration: 0.6s;
+    background-color: #0066ff;
+
+}
+
+input{
+    height: 51px;
+    border: none;
+    border-bottom: 2px solid black;
+    font-size: 20px;
+    opacity: 0.8;
+}
 </style>
