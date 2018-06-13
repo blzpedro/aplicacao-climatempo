7e5e675bc834cc801a6858b1d025d525f4139ed1
Pedro Melo <phenriqmelo99@gmail.com>
2018-06-13 13:26:54 +0000

    CSS Pág. Inicial

--- src/App.vue
+++ src/App.vue
@@ -11,4 +11,14 @@ export default {
 </script>
 
 <style>
+*{
+box-sizing: border-box;
+}
+body {
+font-family: Helvetica, Verdana;
+margin: 0;
+background-image: url("img/ceu.png");
+background-size: 100%;
+background-repeat: no-repeat;
+}
 </style>
